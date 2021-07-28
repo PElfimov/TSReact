2682dca9b816577604940dab1ddd76e884aa3b2a
Pavel Elfimov <pelfimov@yandex.ru>
2021-07-28 22:45:17 +0700

    add some changes

--- src/components/simple/simple.tsx
+++ src/components/simple/simple.tsx
@@ -7,7 +7,36 @@ interface Props {}
 
 @observer
 export class Simple extends Component<Props, {}> {
+  constructor(props: any) {
+    super(props);
+    makeObservable(this);
+  }
+
+  @observable
+  private text: "Hello" | "Goodby" = "Hello";
+
+  @action
+  onClick() {
+    console.log(`this.text = "Hello"`, this.text === "Hello");
+
+    if (this.text === "Hello") {
+      this.text = "Goodby";
+    } else {
+      this.text = "Hello";
+    }
+  }
+
   render() {
-    return <h1>Hello</h1>;
+    return (
+      <>
+        <h1>{this.text}</h1>;
+        <button
+          onClick={() => {
+            this.onClick();
+          }}>
+          Click to bottom
+        </button>
+      </>
+    );
   }
 }
