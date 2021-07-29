fc8bed3797378d4e94a1478c20554d399ecfee85
Pavel Elfimov <pelfimov@yandex.ru>
2021-07-29 14:29:00 +0700

    to do some examples

--- src/components/simple/simple.tsx
+++ src/components/simple/simple.tsx
@@ -14,6 +14,7 @@ export class Simple extends Component<Props, {}> {
 
   @observable
   private text: "Hello" | "Goodby" = "Hello";
+  private count: number = 1;
 
   @action
   onClick() {
@@ -24,19 +25,24 @@ export class Simple extends Component<Props, {}> {
     } else {
       this.text = "Hello";
     }
+
+    this.count = this.count + 1;
+    this.count = this.count + 1;
   }
 
   render() {
     return (
-      <>
-        <h1>{this.text}</h1>;
+      <div className={styles.root}>
+        <h1 className={styles.h1}>{this.text}</h1>
+        <h2>{this.count}</h2>
         <button
           onClick={() => {
             this.onClick();
-          }}>
+          }}
+        >
           Click to bottom
         </button>
-      </>
+      </div>
     );
   }
 }
